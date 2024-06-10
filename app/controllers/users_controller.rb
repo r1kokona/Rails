e69e6ba05376561d123c9a721a69c12501a9bfa9
2402043 <>
2024-06-10 14:51:38 +0900

    add edit & update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
     def index
         @users = User.all
     end
@@ -9,15 +9,27 @@ class UserController < ApplicationController
         @user = User.new
     end
     def create
-        @user = User.new(name:"...")
+        @user = User.new(users_params)
         if @user.save
             redirect_to @user
         else
             render :new, status: :unprocessable_entity
         end
     end
+    def edit
+        @user = User.find(params[:id])
+    end
+    def update
+        @user = User.find(params[:id])
+        if @user.update(users_params)
+            redirect_to @user
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    
     private
-        def user_params
+        def users_params
             params.require(:user).permit(:name)
         end
 end
