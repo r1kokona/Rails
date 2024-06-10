b806b4efdfaecc41f53ddb8bb0ec11217d1b63b7
2402043 <>
2024-06-10 11:36:28 +0900

    rails db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_10_015110) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_10_023454) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "user"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
