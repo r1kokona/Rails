c7b0d7136e5295d8b8ed7087ef6b3bfd074a6476
2402043 <>
2024-06-12 16:37:16 +0900

    # resouces :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "users#index"
+  # resouces :users
   get '/users', to:'uers#index', as:'users'
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
