5bb4e1008a48ee8ca26faf098e728ca61262cf8d
2402043 <>
2024-06-12 11:16:26 +0900

    resouces: users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: "users#index"
-  resources :users
+  get '/users', to:'uers#index', as:'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  put '/users/:id', to: 'users#update'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
 end
