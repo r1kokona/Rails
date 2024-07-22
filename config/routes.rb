15aa10899f21d44cdcda0a010e797836e77a2ad5
2402043 <>
2024-07-22 12:35:57 +0900

    usersリソースを利用

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: "users#index"
-  # resouces :users
-  get '/users', to:'uers#index', as:'users'
-  get '/users/new', to: 'users#new', as: 'new_user'
-  post '/users', to: 'users#create'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
-  put '/users/:id', to: 'users#update'
-  patch '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+  resources :users
+  # get '/users', to:'uers#index', as:'users'
+  # get '/users/new', to: 'users#new', as: 'new_user'
+  # post '/users', to: 'users#create'
+  # get '/users/:id', to: 'users#show', as: 'user'
+  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # put '/users/:id', to: 'users#update'
+  # patch '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
 end
